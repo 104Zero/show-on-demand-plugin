4dd074821e87fdded9707a56881e3c50c6da956a
104Zero Software <software@104zero.com>
2024-10-20 17:34:03 -0700

    PHP8 update gpt

--- show_on_demand.php
+++ show_on_demand.php
@@ -1,181 +1,171 @@
 <?php
-include_once "/opt/fpp/www/common.php"; //Alows use of FPP Functions
+include_once "/opt/fpp/www/common.php"; // Allows use of FPP Functions
 $pluginName = basename(dirname(__FILE__));
-$pluginConfigFile = $settings['configDirectory'] ."/plugin." .$pluginName; //gets path to configuration files for plugin
-    
+$pluginConfigFile = $settings['configDirectory'] . "/plugin." . $pluginName; // Gets path to configuration files for plugin
+
+
 if (file_exists($pluginConfigFile)) {
-	$pluginSettings = parse_ini_file($pluginConfigFile);
+    $pluginSettings = parse_ini_file($pluginConfigFile, false, INI_SCANNER_TYPED); // Added INI_SCANNER_TYPED to parse with type handling
 }
 
 $madeChange = false;
 
-if (strlen(urldecode($pluginSettings['show_on_demand_enabled']))<1){
-	WriteSettingToFile("show_on_demand_enabled",urlencode("false"),$pluginName);
-	$madeChange = true;
+// Ensure $pluginSettings array exists and check if settings are not set or empty
+if (empty($pluginSettings['show_on_demand_enabled'])) {
+    WriteSettingToFile("show_on_demand_enabled", urlencode("false"), $pluginName);
+    $madeChange = true;
 }
 
-if (strlen(urldecode($pluginSettings['on_demand_playlist']))<1){
-	WriteSettingToFile("on_demand_playlist","",$pluginName);
-	$madeChange = true;
+if (empty($pluginSettings['on_demand_playlist'])) {
+    WriteSettingToFile("on_demand_playlist", "", $pluginName);
+    $madeChange = true;
 }
 
-if (strlen(urldecode($pluginSettings['main_playlist']))<1){
-	WriteSettingToFile("main_playlist","",$pluginName);
-	$madeChange = true;
+if (empty($pluginSettings['main_playlist'])) {
+    WriteSettingToFile("main_playlist", "", $pluginName);
+    $madeChange = true;
 }
 
-if (strlen(urldecode($pluginSettings['voipms_api_username']))<1){
-	WriteSettingToFile("voipms_api_username","",$pluginName);
-	$madeChange = true;
+if (empty($pluginSettings['voipms_api_username'])) {
+    WriteSettingToFile("voipms_api_username", "", $pluginName);
+    $madeChange = true;
 }
 
-if (strlen(urldecode($pluginSettings['voipms_api_password']))<1){
-	WriteSettingToFile("voipms_api_password","",$pluginName);
-	$madeChange = true;
+if (empty($pluginSettings['voipms_api_password'])) {
+    WriteSettingToFile("voipms_api_password", "", $pluginName);
+    $madeChange = true;
 }
 
-if (strlen(urldecode($pluginSettings['start_command']))<1){
-	WriteSettingToFile("start_command","START",$pluginName);
-	$madeChange = true;
+if (empty($pluginSettings['start_command'])) {
+    WriteSettingToFile("start_command", "START", $pluginName);
+    $madeChange = true;
 }
 
-if (strlen(urldecode($pluginSettings['message_success']))<1){
-	WriteSettingToFile("message_success","Message received. The show will begin momentarily.",$pluginName);
-	$madeChange = true;
+if (empty($pluginSettings['message_success'])) {
+    WriteSettingToFile("message_success", "Message received. The show will begin momentarily.", $pluginName);
+    $madeChange = true;
 }
 
-if (strlen(urldecode($pluginSettings['message_not_started']))<1){
-	WriteSettingToFile("message_not_started","Sorry, the show cannot be started right now.",$pluginName);
-	$madeChange = true;
+if (empty($pluginSettings['message_not_started'])) {
+    WriteSettingToFile("message_not_started", "Sorry, the show cannot be started right now.", $pluginName);
+    $madeChange = true;
 }
 
-if (strlen(urldecode($pluginSettings['random_playlist_item']))<1){
-	WriteSettingToFile("random_playlist_item",urlencode("false"),$pluginName);
-	$madeChange = true;
+if (empty($pluginSettings['random_playlist_item'])) {
+    WriteSettingToFile("random_playlist_item", urlencode("false"), $pluginName);
+    $madeChange = true;
 }
 
 if ($madeChange) {
-	$pluginSettings = parse_ini_file($pluginConfigFile);
+    $pluginSettings = parse_ini_file($pluginConfigFile, false, INI_SCANNER_TYPED); // Re-read config after changes
 }
 
-$playlists = Array();
-foreach(scandir($playlistDirectory) as $pFile)
-    if ($pFile != "." && $pFile != "..") {
-        if (preg_match('/\.json$/', $pFile))  {
+$playlists = [];
+if (is_dir($playlistDirectory)) {
+    foreach (scandir($playlistDirectory) as $pFile) {
+        if ($pFile != "." && $pFile != ".." && preg_match('/\.json$/', $pFile)) {
             $pFile = preg_replace('/\.json$/', '', $pFile);
             $playlists[$pFile] = $pFile;
         }
     }
+}
 ?>
-
 
 <!DOCTYPE html>
 <html>
 <head>
-
 </head>
 <body>
 <div class="pluginBody" style="margin-left: 1em;">
-	<div class="title">
-		<h1>Show On Demand</h1>
-		<h4></h4>
-	</div>
+    <div class="title">
+        <h1>Show On Demand</h1>
+        <h4></h4>
+    </div>
 
 <p>Press F1 for setup instructions</p>
 <table cellspacing="5">
 <tr>
-	<th style="text-align: left">Enable Show On Demand</th>
-<td>
-<?			
-	PrintSettingCheckbox("Show On Demand", "show_on_demand_enabled", $restart = 1, $reboot = 0, "true", "false", $pluginName = $pluginName, $callbackName = "", $defaultValue = 0, $desc = "", $sData = Array());
-?>
-</td>
-</tr>
-
-
-<tr>
-	<th style="text-align: left">Voip.ms API Username</th>
-<td>
-<?
-//function PrintSettingTextSaved($setting, $restart = 1, $reboot = 0, $maxlength = 32, $size = 32, $pluginName = "", $defaultValue = "", $callbackName = "", $changedFunction = "", $inputType = "text", $sData = Array())
-	PrintSettingTextSaved("voipms_api_username", $restart = 1, $reboot = 0, $maxlength = 32, $size = 32, $pluginName = $pluginName, $defaultValue = "");
-?>
-</td>
+    <th style="text-align: left">Enable Show On Demand</th>
+    <td>
+    <?php
+        PrintSettingCheckbox("Show On Demand", "show_on_demand_enabled", 1, 0, "true", "false", $pluginName, "", 0, "", []);
+    ?>
+    </td>
 </tr>
 
 <tr>
-	<th style="text-align: left">Voip.ms API Password</th>
-<td>
-<?
-//function PrintSettingPasswordSaved($setting, $restart = 1, $reboot = 0, $maxlength = 32, $size = 32, $pluginName = "", $defaultValue = "", $callbackName = "", $changedFunction = "")
-	PrintSettingPasswordSaved("voipms_api_password", $restart = 1, $reboot = 0, $maxlength = 50, $size = 32, $pluginName = $pluginName, $defaultValue = "");
-?>
-</td>
-</tr>
-
-
-<tr>
-	<th style="text-align: left">Start Command</th>
-<td>
-<?
-//function PrintSettingTextSaved($setting, $restart = 1, $reboot = 0, $maxlength = 32, $size = 32, $pluginName = "", $defaultValue = "", $callbackName = "", $changedFunction = "", $inputType = "text", $sData = Array())
-	PrintSettingTextSaved("start_command", $restart = 1, $reboot = 0, $maxlength = 32, $size = 32, $pluginName = $pluginName, $defaultValue = "");
-?>
-</td>
+    <th style="text-align: left">Voip.ms API Username</th>
+    <td>
+    <?php
+        PrintSettingTextSaved("voipms_api_username", 1, 0, 32, 32, $pluginName, "");
+    ?>
+    </td>
 </tr>
 
 <tr>
-	<th style="text-align: left">Success Message</th>
-<td>
-<?
-//function PrintSettingTextSaved($setting, $restart = 1, $reboot = 0, $maxlength = 32, $size = 32, $pluginName = "", $defaultValue = "", $callbackName = "", $changedFunction = "", $inputType = "text", $sData = Array())
-	PrintSettingTextSaved("message_success", $restart = 1, $reboot = 0, $maxlength = 160, $size = 100, $pluginName = $pluginName, $defaultValue = "");
-?>
-</td>
-</tr>
-
-
-<tr>
-	<th style="text-align: left">Not started message</th>
-<td>
-<?
-//function PrintSettingTextSaved($setting, $restart = 1, $reboot = 0, $maxlength = 32, $size = 32, $pluginName = "", $defaultValue = "", $callbackName = "", $changedFunction = "", $inputType = "text", $sData = Array())
-	PrintSettingTextSaved("message_not_started", $restart = 1, $reboot = 0, $maxlength = 160, $size = 100, $pluginName = $pluginName, $defaultValue = "");
-?>
-</td>
+    <th style="text-align: left">Voip.ms API Password</th>
+    <td>
+    <?php
+        PrintSettingPasswordSaved("voipms_api_password", 1, 0, 50, 32, $pluginName, "");
+    ?>
+    </td>
 </tr>
 
 <tr>
-	<th style="text-align: left">On-Demand Playlist</th>
-<td>
-<?
-	// function PrintSettingSelect($title, $setting, $restart = 1, $reboot = 0, $defaultValue, $values, $pluginName = "", $callbackName = "", $changedFunction = "", $sData = Array())
-	PrintSettingSelect("On-Demand Playlist", "on_demand_playlist", $restart = 1, $reboot = 0, "", $playlists, $pluginName);
-?>
-</td>
+    <th style="text-align: left">Start Command</th>
+    <td>
+    <?php
+        PrintSettingTextSaved("start_command", 1, 0, 32, 32, $pluginName, "");
+    ?>
+    </td>
 </tr>
 
 <tr>
-	<th style="text-align: left">Show Playlist</th>
-<td>
-<?
-	PrintSettingSelect("Show Playlist", "main_playlist", $restart = 1, $reboot = 0, "", $playlists, $pluginName);
-?>
-</td>
+    <th style="text-align: left">Success Message</th>
+    <td>
+    <?php
+        PrintSettingTextSaved("message_success", 1, 0, 160, 100, $pluginName, "");
+    ?>
+    </td>
 </tr>
 
 <tr>
-	<th style="text-align: left">Select Random Item from Playlist</th>
-<td>
-<?			
-	PrintSettingCheckbox("Randomize Single Playlist Item", "random_playlist_item", $restart = 1, $reboot = 0, "true", "false", $pluginName = $pluginName, $callbackName = "", $defaultValue = 0, $desc = "", $sData = Array());
-?>
-</td>
+    <th style="text-align: left">Not started message</th>
+    <td>
+    <?php
+        PrintSettingTextSaved("message_not_started", 1, 0, 160, 100, $pluginName, "");
+    ?>
+    </td>
 </tr>
-	
+
+<tr>
+    <th style="text-align: left">On-Demand Playlist</th>
+    <td>
+    <?php
+        PrintSettingSelect("On-Demand Playlist", "on_demand_playlist", 1, 0, "", $playlists, $pluginName);
+    ?>
+    </td>
+</tr>
+
+<tr>
+    <th style="text-align: left">Show Playlist</th>
+    <td>
+    <?php
+        PrintSettingSelect("Show Playlist", "main_playlist", 1, 0, "", $playlists, $pluginName);
+    ?>
+    </td>
+</tr>
+
+<tr>
+    <th style="text-align: left">Select Random Item from Playlist</th>
+    <td>
+    <?php
+        PrintSettingCheckbox("Randomize Single Playlist Item", "random_playlist_item", 1, 0, "true", "false", $pluginName, "", 0, "", []);
+    ?>
+    </td>
+</tr>
+
 </table>
-
-
-
+</div>
 </body>
 </html>
--- show_on_demand_bg.php
+++ show_on_demand_bg.php
@@ -1,65 +1,61 @@
 <?php
-include_once "/opt/fpp/www/common.php";
+include_once "/opt/fpp/www/common.php"; // Import FPP Functions
 $pluginName = basename(dirname(__FILE__));
-$pluginPath = $settings['pluginDirectory']."/".$pluginName."/"; 
+$pluginPath = $settings['pluginDirectory'] . "/" . $pluginName . "/"; 
 
-$logFile = $settings['logDirectory']."/".$pluginName.".log";
-$messagesCsvFile = $settings['logDirectory']."/".$pluginName."-messages.csv";
-$pluginConfigFile = $settings['configDirectory'] . "/plugin." .$pluginName;
-$pluginSettings = parse_ini_file($pluginConfigFile);
+$logFile = $settings['logDirectory'] . "/" . $pluginName . ".log";
+$messagesCsvFile = $settings['logDirectory'] . "/" . $pluginName . "-messages.csv";
+$pluginConfigFile = $settings['configDirectory'] . "/plugin." . $pluginName;
+$pluginSettings = parse_ini_file($pluginConfigFile, false, INI_SCANNER_TYPED); // Updated for typed INI parsing
 
-#$pluginVersion = urldecode($pluginSettings['pluginVersion']);
-$sodEnabled = $pluginSettings['show_on_demand_enabled'];
-$sodEnabled = $sodEnabled == "true" ? true : false;
-$randomItemEnabled = $pluginSettings['random_playlist_item'];
-$randomItemEnabled = $randomItemEnabled  == "true" ? true : false;
+// Assign variables with strict type checking
+$sodEnabled = filter_var($pluginSettings['show_on_demand_enabled'], FILTER_VALIDATE_BOOLEAN);
+$randomItemEnabled = filter_var($pluginSettings['random_playlist_item'], FILTER_VALIDATE_BOOLEAN);
 
 $api_base_path = "https://voip.ms/api/v1";
 $oldest_message_age = 180;
 
-$onDemandPlaylist = $pluginSettings['on_demand_playlist'];
-$mainPlaylist = $pluginSettings['main_playlist'];
-$voipmsApiUsername = $pluginSettings['voipms_api_username'];
-$voipmsApiPassword = $pluginSettings['voipms_api_password'];
-$startCommand = $pluginSettings['start_command'];
-$messageSuccess = $pluginSettings['message_success'];
-$messageNotStarted = $pluginSettings['message_not_started'];
-	
-if($sodEnabled == 1) {
+$onDemandPlaylist = $pluginSettings['on_demand_playlist'] ?? '';
+$mainPlaylist = $pluginSettings['main_playlist'] ?? '';
+$voipmsApiUsername = $pluginSettings['voipms_api_username'] ?? '';
+$voipmsApiPassword = $pluginSettings['voipms_api_password'] ?? '';
+$startCommand = $pluginSettings['start_command'] ?? '';
+$messageSuccess = $pluginSettings['message_success'] ?? '';
+$messageNotStarted = $pluginSettings['message_not_started'] ?? '';
+
+if ($sodEnabled) {
     echo "Starting Show On Demand Plugin\n";
     logEntry("Starting Show On Demand Plugin");
 
-    try{
+    try {
+        // Validate settings
         logEntry("On-demand playlist: " . $onDemandPlaylist);
-        if (strlen($onDemandPlaylist)==0){
+        if (empty($onDemandPlaylist)) {
             throw new Exception('No on-demand playlist specified.');
         }
 
         logEntry("Main playlist: " . $mainPlaylist);
-        if (strlen($mainPlaylist)==0){
+        if (empty($mainPlaylist)) {
             throw new Exception('No main playlist specified.');
         }
 
         logEntry("Voip.ms username: " . $voipmsApiUsername);
-        if (strlen($voipmsApiUsername)==0){
+        if (empty($voipmsApiUsername)) {
             throw new Exception('No voip.ms username specified.');
         }
 
-        logEntry("Voip.ms password: " . "<<redacted>>");
-        if (strlen($voipmsApiPassword)==0){
+        logEntry("Voip.ms password: <<redacted>>");
+        if (empty($voipmsApiPassword)) {
             throw new Exception('No voip.ms password specified.');
         }
 
         logEntry("Start command: " . $startCommand);
-        if (strlen($startCommand)==0){
+        if (empty($startCommand)) {
             throw new Exception('No start command specified.');
         }
 
-        logEntry("Random Item " . $randomItemEnabled);
-        if ($randomItemEnabled==0){
-            throw new Exception('Random Item not specified.');
-        }
-	    
+        logEntry("Random Item: " . $randomItemEnabled);
+
         logEntry("Success message: " . $messageSuccess);
         logEntry("Not-started message: " . $messageNotStarted);
 
@@ -68,17 +64,17 @@ if($sodEnabled == 1) {
         die;
     }
 
-    while(true) {
-        try{
+    while (true) {
+        try {
             $fppStatus = getFppStatus();
 
-            if($fppStatus->scheduler->status=="playing") {
+            if ($fppStatus->scheduler->status === "playing") {
                 logEntry("fpp is playing");
 
                 $messageResponse = getMessages();
                 logEntry("API Response Status: " . $messageResponse->status);
 
-                if ($messageResponse->status == "success"){
+                if ($messageResponse->status === "success") {
                     $startShowForContacts = processMessages($messageResponse);
 
                     $shouldStart = count($startShowForContacts) > 0;
@@ -86,28 +82,20 @@ if($sodEnabled == 1) {
                     if ($shouldStart) {
                         $currentlyPlaying = $fppStatus->current_playlist->playlist;
                         logEntry("Currently playing: " . $currentlyPlaying);
-        
-                        $canStart = false;
-                        if($currentlyPlaying == $onDemandPlaylist) {
-                            logEntry("The on-demand playlist is playing"); 
-                            $canStart = true;
-                        }
-                        else{
-                            logEntry("The on-demand playlist is not playing");
-                            $canStart = false;
-                        }
 
-                        sendResponses($startShowForContacts,$canStart);
+                        $canStart = ($currentlyPlaying === $onDemandPlaylist);
+                        logEntry($canStart ? "The on-demand playlist is playing" : "The on-demand playlist is not playing");
 
-                        if ($canStart){
+                        sendResponses($startShowForContacts, $canStart);
+
+                        if ($canStart) {
                             startShow();
                         }
-                    }
-                    else{
+                    } else {
                         logEntry("Nothing to do");
                     }
                 }
-            }else {
+            } else {
                 logEntry("fpp is not playing");
             }
         } catch (Exception $e) {
@@ -117,37 +105,29 @@ if($sodEnabled == 1) {
         logEntry("Sleeping");
         sleep(30);
     }
-}else {
+} else {
     logEntry("Show On Demand Plugin is disabled");
 }
 
-function startShow(){
-    global $mainPlaylist;
-    global $randomItemEnabled;
+function startShow() {
+    global $mainPlaylist, $randomItemEnabled;
 
-    if ($randomItemEnabled == 1){
-	$url = "http://127.0.0.1/api/command/Insert Random Item From Playlist/" . $mainPlaylist ."/true";
-    }
-    else{	    
-    	$url = "http://127.0.0.1/api/command/Insert Playlist Immediate/" . $mainPlaylist ."/0/0/true";
-    }
-	
-    $url = str_replace(' ', '%20', $url);
+    $url = $randomItemEnabled 
+        ? "http://127.0.0.1/api/command/Insert Random Item From Playlist/" . urlencode($mainPlaylist) . "/true" 
+        : "http://127.0.0.1/api/command/Insert Playlist Immediate/" . urlencode($mainPlaylist) . "/0/0/true";
 
     logEntry("Triggering main playlist: " . $url);
-    $result=file_get_contents($url);
+    $result = file_get_contents($url);
     logEntry("Result: " . $result);
 }
 
-function processMessages($messageResponse){
+function processMessages($messageResponse) {
     global $startCommand, $oldest_message_age;
 
-    $shouldStart = false;
+    $respondTo = [];
 
-    $respondTo = array();
-
-    foreach($messageResponse->sms as $item) {
-        try{
+    foreach ($messageResponse->sms as $item) {
+        try {
             $id = $item->id;
             $date = $item->date;
             $did = $item->did;
@@ -158,32 +138,20 @@ function processMessages($messageResponse){
             $action = "ignored";
 
             $now = new DateTime('now');
-            $datetime = new DateTime( $date );
+            $datetime = new DateTime($date);
             $diffInSeconds = $now->getTimestamp() - $datetime->getTimestamp();
             logEntry("Message Age: " . $diffInSeconds);
 
-            if ($diffInSeconds > $oldest_message_age){
+            if ($diffInSeconds > $oldest_message_age) {
                 $action = "too old";
+            } else if (strcasecmp($message, $startCommand) === 0) {
+                $action = "start show";
+                $respondTo[$contact] = $did;
+            } else {
+                logEntry("Unknown message: " . $message);
             }
-            else {
-                if (strcasecmp($message, $startCommand) == 0) {
-                    $action = "start show";
-                    if ($shouldStart){
-                        $action = "start show (duplicate)";
-                    }
-                    $shouldStart = true;
-                    //array_push($respondTo, $contact);
-                    $respondTo[$contact] = $did;
-                }
-                else{
-                    logEntry("Unknown message: " . $message);
-                }
-            }
-
-
 
             saveMessageToCsv($id, $date, $did, $contact, $message, $action);
-
             deleteMessage($id);
 
         } catch (Exception $e) {
@@ -196,124 +164,99 @@ function processMessages($messageResponse){
     return $respondTo;
 }
 
-function sendResponses($contacts, $didStart){
-    global $messageNotStarted,$messageSuccess;
-    foreach($contacts as $destination => $did) {
-        logEntry("Destination: " . $destination . "  DID: " . $did);
-        
-        $message = $messageNotStarted;
-        if ($didStart){
-            $message = $messageSuccess;
-        }
+function sendResponses($contacts, $didStart) {
+    global $messageNotStarted, $messageSuccess;
 
+    foreach ($contacts as $destination => $did) {
+        logEntry("Destination: " . $destination . "  DID: " . $did);
+
+        $message = $didStart ? $messageSuccess : $messageNotStarted;
         sendMessage($did, $destination, $message);
     }
 }
 
-function getMessages(){
-    global $api_base_path,$voipmsApiUsername,$voipmsApiPassword;
+function getMessages() {
+    global $api_base_path, $voipmsApiUsername, $voipmsApiPassword;
+
     $url = $api_base_path . "/rest.php";
-    $options = array(
-        'http' => array(
-        'method'  => 'GET'
-        )
-    );
-    $getdata = http_build_query(
-        array(
+    $getdata = http_build_query([
         'api_username' => $voipmsApiUsername,
         'api_password' => $voipmsApiPassword,
-        'method'=>'getSMS',
-        'type'=>'1',
-         )
-    );
-    $context = stream_context_create( $options );
-    //logEntry("API Request: " . $url ."?" .$getdata);
-    $result = file_get_contents( $url ."?" .$getdata, false, $context );
+        'method' => 'getSMS',
+        'type' => '1'
+    ]);
+
+    logEntry("API Request: " . $url . "?" . $getdata);
+    $result = file_get_contents($url . "?" . $getdata);
     logEntry("API response: " . $result);
-    return json_decode( $result );
+
+    return json_decode($result);
 }
 
-function deleteMessage($id){
-    global $api_base_path,$voipmsApiUsername,$voipmsApiPassword;
+function deleteMessage($id) {
+    global $api_base_path, $voipmsApiUsername, $voipmsApiPassword;
+
     $url = $api_base_path . "/rest.php";
-    $options = array(
-        'http' => array(
-        'method'  => 'GET'
-        )
-    );
-    $getdata = http_build_query(
-        array(
+    $getdata = http_build_query([
         'api_username' => $voipmsApiUsername,
         'api_password' => $voipmsApiPassword,
-        'method'=>'deleteSMS',
-        'id'=>$id,
-         )
-    );
+        'method' => 'deleteSMS',
+        'id' => $id
+    ]);
+
     logEntry("Deleting SMS ID: " . $id);
-    $context = stream_context_create( $options );
-    //logEntry("API Request: " . $url ."?" .$getdata);
-    $result = file_get_contents( $url ."?" .$getdata, false, $context );
+    $result = file_get_contents($url . "?" . $getdata);
     logEntry("API response: " . $result);
-    return json_decode( $result );
+
+    return json_decode($result);
 }
 
-function sendMessage($did, $destination, $message){
-    global $api_base_path,$voipmsApiUsername,$voipmsApiPassword;
+function sendMessage($did, $destination, $message) {
+    global $api_base_path, $voipmsApiUsername, $voipmsApiPassword;
+
     $url = $api_base_path . "/rest.php";
-    $options = array(
-        'http' => array(
-        'method'  => 'GET'
-        )
-    );
-    $getdata = http_build_query(
-        array(
+    $getdata = http_build_query([
         'api_username' => $voipmsApiUsername,
         'api_password' => $voipmsApiPassword,
-        'method'=>'sendSMS',
-        'did'=>$did,
-        'dst'=>$destination,
-        'message'=>$message
-         )
-    );
+        'method' => 'sendSMS',
+        'did' => $did,
+        'dst' => $destination,
+        'message' => $message
+    ]);
+
     logEntry("Sending SMS to: " . $destination);
-    $context = stream_context_create( $options );
-    //logEntry("API Request: " . $url ."?" .$getdata);
-    $result = file_get_contents( $url ."?" .$getdata, false, $context );
+    $result = file_get_contents($url . "?" . $getdata);
     logEntry("API response: " . $result);
-    return json_decode( $result );
+
+    return json_decode($result);
 }
 
 function getFppStatus() {
-    $result=file_get_contents("http://127.0.0.1/api/fppd/status");
-    return json_decode( $result );
-  }
+    $result = file_get_contents("http://127.0.0.1/api/fppd/status");
+    return json_decode($result);
+}
 
 function logEntry($data) {
+    global $logFile, $myPid;
 
-	global $logFile,$myPid;
-
-	$data = $_SERVER['PHP_SELF']." : [".$myPid."] ".$data;
-	
-	$logWrite= fopen($logFile, "a") or die("Unable to open file!");
-	fwrite($logWrite, date('Y-m-d h:i:s A',time()).": ".$data."\n");
-	fclose($logWrite);
+    $data = $_SERVER['PHP_SELF'] . " : [" . $myPid . "] " . $data;
+    $logWrite = fopen($logFile, "a") or die("Unable to open file!");
+    fwrite($logWrite, date('Y-m-d h:i:s A') . ": " . $data . "\n");
+    fclose($logWrite);
 }
 
 function saveMessageToCsv($id, $date, $did, $contact, $message, $action) {
-
     global $messagesCsvFile;
-    
+
     if (!file_exists($messagesCsvFile)) {
-        $csvHeaderWrite= fopen($messagesCsvFile, "a") or die("Unable to open file!");
-        fwrite($csvHeaderWrite, "id,date,did,contact,message,action" ."\n");
+        $csvHeaderWrite = fopen($messagesCsvFile, "a") or die("Unable to open file!");
+        fwrite($csvHeaderWrite, "id,date,did,contact,message,action\n");
         fclose($csvHeaderWrite);
     }
 
-    $esc = str_replace("\"","\"\"",$message);
-	
-	$csvWrite= fopen($messagesCsvFile, "a") or die("Unable to open file!");
-	fwrite($csvWrite, $id . "," . $date . "," . $did . "," . $contact . "," . "\"" . $esc . "\"" . "," . $action . "\n");
-	fclose($csvWrite);
+    $esc = str_replace("\"", "\"\"", $message);
+    $csvWrite = fopen($messagesCsvFile, "a") or die("Unable to open file!");
+    fwrite($csvWrite, $id . "," . $date . "," . $did . "," . $contact . "," . "\"" . $esc . "\"" . "," . $action . "\n");
+    fclose($csvWrite);
 }
-
 ?>
