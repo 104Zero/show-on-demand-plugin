e1492cc6c5b0cb59a961521d042e83c66f961a42
104Zero <77141040+104Zero@users.noreply.github.com>
2024-10-21 13:19:55 -0700

    Update postStart.sh

--- scripts/postStart.sh
+++ scripts/postStart.sh
@@ -2,6 +2,7 @@
 
 echo "Running SOD PreStart Script"
 
+/usr/bin/php /home/fpp/media/plugins/show-on-demand-edit/show_on_demand_bg.php &
 #sudo chmod 755 /home/fpp/media/plugins/show-on-demand-edit/show_on_demand_bg.php 
 
 
